c9ae1f1198a5794bffb32bf87471f355d068f989
LiyaMio <62882186+LiyaMio@users.noreply.github.com>
2021-10-06 16:44:41 +0800

    1

--- JS/echarts.js
+++ JS/echarts.js
@@ -92,3 +92,50 @@ function setMemory(){
     };
     option && myChart.setOption(option);
 }
+function setVariance(){
+    var chartDom = document.getElementById('variance');
+    var myChart = echarts.init(chartDom);
+    var option;
+    option = {
+        title:{
+            // left:"5%",
+            text:'内存使用对比'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        dataset:{
+            source: [
+                ['dataSize', '10000', '20000', '30000', '40000', '50000', '100000'],
+                ['newA', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+                ['oldA', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+
+            ]
+        },
+        legend: {
+            data: ['newA', 'oldA']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+        ]
+    };
+    option && myChart.setOption(option);
+}
--- JS/map.js
+++ JS/map.js
@@ -1,35 +1,22 @@
-
-function test(){
+function test() {
     alert("test succcess!");
 }
 
+$(document).ready(() => {
+    addDatasetBtn()
+})
+
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-    var tmp=[];
+    var tmp = [];
     tmp.push([47.6149, -122.1941]);
     tmp.push([46.6149, -122.1941]);
     tmp.push([45.6149, -122.1941]);
     tmp.push([44.6149, -122.1941]);
     tmp.push([43.6149, -122.1941]);
     //Create custom Pushpin
-    for(var i=0;i<5;++i) {
-        var center =  new Microsoft.Maps.Location(tmp[i][0],tmp[i][1]);
-        var pin = new Microsoft.Maps.Pushpin(center, {
-             // color:purple
-        });
-
-        //Add the pushpin to the map
-        map.entities.push(pin);
-    }
-    var bmp=[];
-    bmp.push([47.6149, -123.1941]);
-    bmp.push([46.6149, -123.1941]);
-    bmp.push([45.6149, -123.1941]);
-    bmp.push([44.6149, -123.1941]);
-    bmp.push([43.6149, -123.1941]);
-    //Create custom Pushpin
-    for(var i=0;i<5;++i) {
-        var center =  new Microsoft.Maps.Location(bmp[i][0],bmp[i][1]);
+    for (var i = 0; i < 5; ++i) {
+        var center = new Microsoft.Maps.Location(tmp[i][0], tmp[i][1]);
         var pin = new Microsoft.Maps.Pushpin(center, {
             // color:purple
         });
@@ -37,10 +24,91 @@ function loadMapScenario() {
         //Add the pushpin to the map
         map.entities.push(pin);
     }
-    // var pushpins =Microsoft.Maps.TestDataGenerator.getPushpins(10, map.getBounds());
-    // var layer = new Microsoft.Maps.Layer();
-    // layer.add(pushpins);
-    // map.layers.insert(layer);
+    var bmp = [];
+    bmp.push([47.6149, -123.1941]);
+    bmp.push([46.6149, -123.1941]);
+    bmp.push([45.6149, -123.1941]);
+    bmp.push([44.6149, -123.1941]);
+    bmp.push([43.6149, -123.1941]);
+    //Create custom Pushpin
+    for (var i = 0; i < 5; ++i) {
+        var center = new Microsoft.Maps.Location(bmp[i][0], bmp[i][1]);
+        var pin = new Microsoft.Maps.Pushpin(center, {
+            // color:purple
+        });
+
+        //Add the pushpin to the map
+        map.entities.push(pin);
+    }
+    var center = map.getCenter();
+
+    //Create array of locations
+    var coords = [new Microsoft.Maps.Location(47.6149, -123.1941), new Microsoft.Maps.Location(46.6149, -123.1941)];
+
+    //Create a polyline
+    var line = new Microsoft.Maps.Polyline(coords, {
+        strokeColor: 'red',
+        strokeThickness: 3,
+        strokeDashArray: [4, 4]
+    });
+
+    //Add the polyline to map
+    map.entities.push(line);
+
 
 }
 
+function addDatasetBtn(val,text) {
+    for (var i = 0; i < 10; ++i) {
+        var id = 'datasetoption', val = 'dataset', text = 'dataset';
+        id = id + i.toString()
+        text = text + i.toString()
+        var select_template = `<input type="radio" class="btn-check" name="btnradio" id="${id}" autocomplete="off" value="${val}"><label class="btn btn-outline-primary" for="${id}">${text}</label>`
+        console.log(select_template)
+        // var vv = document.createElement('div');
+        // vv.innerHTML = select_template;
+        // document.getElementById("btnContainer").appendChild(vv)
+        document.getElementById("btnContainer").innerHTML+=select_template
+    }
+}
+function chooseData(){
+    var tmp
+    for(var i=0;i<10;++i)
+    {
+        if(document.getElementsByName(bthradio).checked)
+            tmp=documen.getElementById()
+    }
+    $.ajax({
+        url:'',
+        type:'POST',
+        dataType:'json',
+        data:tmp,
+        success:function (data) {
+            console.log(data)
+
+        }
+    })
+}
+
+// function getLocal(){
+//     $.ajax({
+//         url:'',
+//         typr:'GET',
+//         dataType:'json',
+//         success:function(datasset)
+//         {
+//             var data;
+//             for(var i=0;i<50;++i)
+//             {
+//                 tmp[i]=new Microsoft.Maps.Location(datasset[i].longitude,datasset[i].latitude);
+//             }
+//             var line = new Microsoft.Maps.Polyline(data, {
+//                 strokeColor: 'red',
+//                 strokeThickness: 3,
+//                 strokeDashArray: [4, 4]
+//             });
+//             map.entities.push(line);
+//         }
+//     })
+//
+// }
