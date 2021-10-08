2619eec152bf49e4f21e7bfa2a84afa4fae82da6
LiyaMio <62882186+LiyaMio@users.noreply.github.com>
2021-10-08 08:04:16 +0800

    add charts

--- JS/echarts.js
+++ JS/echarts.js
@@ -99,7 +99,148 @@ function setVariance(){
     option = {
         title:{
             // left:"5%",
-            text:'内存使用对比'
+            text:'方差对比'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        dataset:{
+            source: [
+                ['dataSize', '10000', '20000', '30000', '40000', '50000', '100000'],
+                ['newA', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+                ['oldA', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+
+            ]
+        },
+        legend: {
+            data: ['newA', 'oldA']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+        ]
+    };
+    option && myChart.setOption(option);
+}
+function test1(){
+    var chartDom = document.getElementById('c1');
+    var myChart = echarts.init(chartDom);
+    var option;
+    option = {
+        title:{
+            // left:"5%",
+            text:'chart 1'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        dataset:{
+            source: [
+                ['dataSize', '10000', '20000', '30000', '40000', '50000', '100000'],
+                ['newA', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+                ['oldA', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+
+            ]
+        },
+        legend: {
+            data: ['newA', 'oldA']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+        ]
+    };
+    option && myChart.setOption(option);
+}
+function test2(){
+    var chartDom = document.getElementById('c2');
+    var myChart = echarts.init(chartDom);
+    var option;
+    option = {
+        title:{
+            // left:"5%",
+            text:'chart 2'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        dataset:{
+            source: [
+                ['dataSize', '10000', '20000', '30000', '40000', '50000', '100000'],
+                ['newA', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+                ['oldA', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+
+            ]
+        },
+        legend: {
+            data: ['newA', 'oldA']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+            {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
+        ]
+    };
+    option && myChart.setOption(option);
+}
+function test3(){
+    var chartDom = document.getElementById('c3');
+    var myChart = echarts.init(chartDom);
+    var option;
+    option = {
+        title:{
+            // left:"5%",
+            text:'chart 3'
         },
         tooltip: {
             trigger: 'axis'
